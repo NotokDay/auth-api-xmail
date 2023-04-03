89a28ff310dbca46f5dc04e0a6b26488970ec8d1
Mahammadjan Mahammadjanov <67225649+m1r3x@users.noreply.github.com>
2023-04-03 21:35:21 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { logger } = require('./utils/logger');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 443;
 
 app.listen(PORT, () => {
     logger.info(`Running on PORT ${PORT}`);
